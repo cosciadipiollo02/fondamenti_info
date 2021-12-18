1df8ca835c481d5cb64ddd2307732e238fe0150e
cosciadipiollo02 <58572007+cosciadipiollo02@users.noreply.github.com>
2021-12-18 19:35:40 +0100

    hsh

---
+++ c/prove d'esame/es3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int a [3][3] n = {{3,4,4}{4,4,5}{4,4,2}};
+
+int sommamatrice(matrice[3][3]);
+
+int main(){
+
+} 
+
+int sommamatrice(matrice[3][3]){
+    int somma = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            somma += matrice [i][j];
+
+        }
+    }
+}
