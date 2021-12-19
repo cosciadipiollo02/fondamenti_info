12f413e1b8e1ff0b758161328b045d25fa9a3f16
Tommaso <tommasopiccoli7575@gmail.com>
2021-12-19 11:32:13 +0100

    tsr

--- c/prove d'esame/es3.c
+++ c/prove d'esame/es3.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#define N 3
 
-int a [3][3] n = {{3,4,4}{4,4,5}{4,4,2}};
+int a [N][N] = {{3,4,4},{4,4,5},{4,4,3}};
 
-int sommamatrice(matrice[3][3]);
+int sommamatrice(int matrice[][N], int dimensione);
+
 
 int main(){
-
+    int sommatoria = sommamatrice(a,3);
+    printf("%d \n", sommatoria);
 } 
 
-int sommamatrice(matrice[3][3]){
-    int somma = 0;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            somma += matrice [i][j];
-
+int sommamatrice(int matrice[][N], int dimensione){
+    int somma = 0 ;
+    if (dimensione == 0){
+        return matrice[0][0] + matrice[0][1] + matrice[0][2];
+    }
+    else {
+        for(int i = 0; i < 3; i++){
+            somma += matrice[dimensione][i];
         }
     }
+    return somma + sommamatrice(matrice , dimensione-1);
+    
+    
 }
