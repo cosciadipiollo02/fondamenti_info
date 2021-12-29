3b351bdfddd8c1b07cb348a3e537d4b983f4677e
tommaso <tommasopiccoli7575@gmail.com>
2021-12-29 14:30:11 +0100

    njksn

---
+++ c/esercitazione5/es2.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#define N 100
+#define M 100
+
+
+int main(){
+    FILE* file = fopen("es2file.txt","r");
+    int righe = getc(file)-'0';
+    fseek(file,1,SEEK_CUR);
+    int colonne = getc(file)- '0';
+    char matrice[N][M];
+
+
+    printf("%d   %d",righe,colonne);
+
+    for(int i = 0; i < righe; i++){
+        for(int j = 0; j < righe; j++){
+
+            fscanf(file,"%c",&matrice[i][j]);
+        }
+    }
+
+    for(int i = 0; i < colonne; i++){
+        for(int j = 0; j < righe; j++){
+           printf("%c",matrice[i][j]); 
+        }
+    }
+
+    
+}
