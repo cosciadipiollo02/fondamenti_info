a8efa22b00110940638da4e26c8699c34ddc6423
tommypic <tommasopiccoli7575@gmail.com>
2022-01-01 18:06:25 +0100

    ciao

--- c/esercitazione5/es3.c
+++ c/esercitazione5/es3.c
@@ -1,1 +1,28 @@
-ciao
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 30
+
+typedef struct nodo{
+        char nome[N];
+        int costo;
+        int quantita;
+        struct nodo *next;
+    } nodo;
+
+void inserisci_elemento(nodo *lista, char *nome, int quantita)
+
+int main(){
+    printf("ciao");
+    nodo *lista = (nodo *) malloc(sizeof(nodo));
+    strcpy(lista->nome, "cipolla");
+    lista->costo = 30;
+    lista->quantita = 4;
+    printf("ciao %d %s", lista->quantita , lista->nome);
+}
+
+
+void inserisci_elemento(nodo *lista, char *nome, int quantita){
+    nodo *temp;
+
+
